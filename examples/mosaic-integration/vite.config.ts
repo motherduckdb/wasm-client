f21a928db7535366f53f0ebf009c7803145c99b9
Jeff Raymakers <jray@motherduck.com>
2024-03-15 12:28:32 -0700

    fix earthquakes

--- examples/mosaic-integration/vite.config.ts
+++ examples/mosaic-integration/vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   experimental: {
-    // Ensures URL for motherduck_logo is relative from index.html (i.e. assets/motherduck_logo) instead of absolute
-    // (i.e. /assets/motherduck_logo).
+    // Ensures asset URLs are relative from index.html
     renderBuiltUrl: (filename) => filename
   },
   plugins: [react()],
