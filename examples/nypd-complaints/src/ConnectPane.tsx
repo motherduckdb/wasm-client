8d192de5e53855082b88b6e656a3214eda0e9593
Jeff Raymakers <jray@motherduck.com>
2024-03-26 16:56:09 -0700

    streamline Firefox/Safari case

--- examples/nypd-complaints/src/ConnectPane.tsx
+++ examples/nypd-complaints/src/ConnectPane.tsx
@@ -16,6 +16,7 @@ export function ConnectPane({
   connect: (token: string) => void;
 }) {
   const [token, setToken] = useState<string | null>(null);
+  const [tokenInClipboard, setTokenInClipboard] = useState<boolean>(false);
 
   const handleTokenInputChange = useCallback((value: string) => {
     setToken(value);
@@ -23,12 +24,17 @@ export function ConnectPane({
 
   const handleGetTokenButtonClick = useCallback(() => {
     const url = new URL(window.location.href);
-    url.searchParams.set('paste', 'y');
+    url.searchParams.set('tokenInClipboard', 'y');
     window.location.href = `${motherDuckUrl}/token-request?appName=${encodeURIComponent(appName)}&returnTo=${encodeURIComponent(url.toString())}`;
   }, []);
 
   const handleConnectButtonClick = useCallback(() => {
     if (token) {
+      const url = new URL(window.location.href);
+      if (url.searchParams.get('tokenInClipboard')) {
+        url.searchParams.delete('tokenInClipboard');
+        history.pushState({}, '', url);
+      }
       connect(token);
     }
   }, [connect, token]);
@@ -36,23 +42,40 @@ export function ConnectPane({
   useEffect(() => {
     async function attemptConnect() {
       const url = new URL(window.location.href);
-      if (url.searchParams.get('paste')) {
-        url.searchParams.delete('paste');
-        history.pushState({}, '', url);
+      if (url.searchParams.get('tokenInClipboard')) {
         // This only works in Chrome. User has to manually paste in other browsers (Firefox, Safari).
-        const token = await navigator.clipboard.readText();
-        if (token) {
-          connect(token);
+        if (navigator.clipboard.readText) {
+          const token = await navigator.clipboard.readText();
+          if (token) {
+            url.searchParams.delete('tokenInClipboard');
+            history.pushState({}, '', url);
+            connect(token);
+            return;
+          }
         }
+        setTokenInClipboard(true);
       }
     }
     attemptConnect();
   }, [connect]);
 
+  const setTextInputRef = useCallback((element: HTMLInputElement | null) => {
+    if (element) {
+      element.focus();
+    }
+  }, []);
+
   return (
     <div id="connect-pane" className="pt-32">
+      <div
+        className="bg-blue-100 text-blue-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
+        style={{ visibility: tokenInClipboard ? 'visible' : 'hidden' }}
+      >
+        Your token is in the clipboard. Paste it and click Connect.
+      </div>
       <div id="token-input-row">
         <TextInput
+          ref={setTextInputRef}
           type="password"
           autoComplete="off"
           placeholder="Paste MotherDuck Service Token Here"
